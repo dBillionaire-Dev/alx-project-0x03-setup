1c9f8bea7d48011e6d016d30a09331479c0858b2
dBillioniare-Dev <nezerekunke@gmail.com>
2025-06-07 22:49:49 +0100

    task_1

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,22 +1,9 @@
-import React from 'react'
-import Layout from '../components/layouts/Layout'
-
-const index = () => {
+const Home = () => {
   return (
     <div>
-      <Layout>
-        <div className="container mx-auto px-4 py-10">
-          <h1 className="text-4xl font-bold mb-6">Welcome to Splash App</h1>
-          <p className="text-lg text-gray-700 mb-4">
-            Splash App is your one-stop platform for all your needs. Connecting people, creating opportunities.
-          </p>
-          <p className="text-lg text-gray-700">
-            Explore our services and discover how we can help you achieve your goals.
-          </p>
-        </div>
-      </Layout>
+      <h1> Welcome to Splash App </h1>
     </div>
   )
 }
 
-export default index
+export default Home;
