ce50532abda389601261b5292fc0058b9754ff56
dBillioniare-Dev <nezerekunke@gmail.com>
2025-06-07 22:59:57 +0100

    task_2

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -6,12 +6,13 @@ interface PageRouteProps {
 }
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
     router.push(pageRoute, undefined, { shallow: false })
   }
+
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
